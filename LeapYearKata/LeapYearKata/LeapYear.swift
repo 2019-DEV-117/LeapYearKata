f10e4cd216f0da8b84237a37609d0de8aea2201a
2019-DEV-117 <>
2019-03-01 16:15:39 +0100

    applied the helper method to isYearDivisibleBy4ButNotBy100 function

--- LeapYearKata/LeapYearKata/LeapYear.swift
+++ LeapYearKata/LeapYearKata/LeapYear.swift
@@ -9,7 +9,7 @@ class LeapYear {
     }
     
     private static func isYearDivisibleBy4ButNotBy100(_ year: Int) -> (Bool) {
-        return ((year % 4 == 0) && !(year % 100 == 0))
+        return isYear(year, divisibleBy: 4) && !isYear(year, divisibleBy: 100)
     }
     
     private static func isYear(_ year:Int, divisibleBy divisor:Int) -> Bool {
