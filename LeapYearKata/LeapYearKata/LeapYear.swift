2567344134186524de04656d297de326d07b8716
2019-DEV-117 <>
2019-03-01 16:14:07 +0100

    helper function to tell if a year is divisible by a number

--- LeapYearKata/LeapYearKata/LeapYear.swift
+++ LeapYearKata/LeapYearKata/LeapYear.swift
@@ -5,10 +5,14 @@ class LeapYear {
     }
     
     private static func isYearDivisibleBy400(_ year: Int) -> (Bool) {
-        return (year % 400 == 0)
+        return  isYear(year, divisibleBy: 400)
     }
     
     private static func isYearDivisibleBy4ButNotBy100(_ year: Int) -> (Bool) {
         return ((year % 4 == 0) && !(year % 100 == 0))
+    }
+    
+    private static func isYear(_ year:Int, divisibleBy divisor:Int) -> Bool {
+        return year % divisor == 0
     }
 }
