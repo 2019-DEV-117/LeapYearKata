87d11358dc2f9af6874bddab44dd22d2cdb6465c
2019-DEV-117 <>
2019-03-01 16:08:12 +0100

    extracted method isYearDivisibleBy4ButNotBy100

--- LeapYearKata/LeapYearKata/LeapYear.swift
+++ LeapYearKata/LeapYearKata/LeapYear.swift
@@ -1,10 +1,14 @@
 class LeapYear {
 
     static func isLeapYear(year:Int) -> Bool {
-        return isYearDivisibleBy400(year) || ((year % 4 == 0) && !(year % 100 == 0))
+        return isYearDivisibleBy400(year) || isYearDivisibleBy4ButNotBy100(year)
     }
     
     private static func isYearDivisibleBy400(_ year: Int) -> (Bool) {
         return (year % 400 == 0)
+    }
+    
+    private static func isYearDivisibleBy4ButNotBy100(_ year: Int) -> (Bool) {
+        return ((year % 4 == 0) && !(year % 100 == 0))
     }
 }
