23e77ce29825bcbad75487d1c0499076f8dc5bad
2019-DEV-117 <>
2019-03-01 15:54:29 +0100

    test for AllYearDivisibleBy100ButNotBy400AreNotLeapYears

--- LeapYearKata/LeapYearTests/LeapYearTests.swift
+++ LeapYearKata/LeapYearTests/LeapYearTests.swift
@@ -5,4 +5,8 @@ class LeapYearTests: XCTestCase {
     func testAllYearDivisibleBy400AreLeapYears()  {
         XCTAssertTrue(LeapYear.isLeapYear(year:2000))
     }
+    
+    func testAllYearDivisibleBy100ButNotBy400AreNotLeapYears()  {
+        XCTAssertFalse(LeapYear.isLeapYear(year:1700))
+    }
 }
