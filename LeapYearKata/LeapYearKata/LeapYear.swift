a54bf8d6a5d676f6505b87e16388d121328ed048
2019-DEV-117 <>
2019-03-01 16:02:36 +0100

    extracted to a method isYearDivisibleBy400

--- LeapYearKata/LeapYearKata/LeapYear.swift
+++ LeapYearKata/LeapYearKata/LeapYear.swift
@@ -1,6 +1,10 @@
 class LeapYear {
-    
+
     static func isLeapYear(year:Int) -> Bool {
-        return (year % 400 == 0) || ((year % 4 == 0) && !(year % 100 == 0))
+        return isYearDivisibleBy400(year) || ((year % 4 == 0) && !(year % 100 == 0))
+    }
+    
+    private static func isYearDivisibleBy400(_ year: Int) -> (Bool) {
+        return (year % 400 == 0)
     }
 }
